9268523cbae68c4dd4e82dd4767066514ff25568
Stefan Rimaila <stefan@rimaila.fi>
2025-05-13 18:41:44 +0300

    chore: cleanup, moving

--- src/app/ref/page.tsx
+++ src/app/ref/page.tsx
@@ -1,4 +1,4 @@
-import ContentPageLayout from '@/layouts/ContentPageLayout';
+// import ContentPageLayout from '@/layouts/ContentPageLayout';
 
 export default function ReferencePage() {
   return <>kontsaa</>;
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,18 +1,10 @@
 'use client';
 
-import Image from 'next/image';
 import useMasonry from '../hooks/useMasonry';
 import Work from './components/Work';
 
 import pictures from '@/data/pictures.json';
 import { works } from '@/schemas/work';
-
-type WorkItem = {
-  src: string;
-  alt: string;
-  id: string;
-  posted?: Date;
-};
 
 const images = works.parse(pictures);
 
@@ -23,23 +15,10 @@ export default function WorksPage() {
     <>
       <main className="container mx-auto">
         <div ref={masonryRef} className="grid items-start gap-4 sm:grid-cols-3">
-          {/* <Masonry columnsCount={3}> */}
-          {images.map(({ src, alt, id, posted }) => (
-            <Work key={id} {...{ src, alt, id, posted }} />
+          {images.map(it => (
+            <Work key={it.id} workItem={it} />
           ))}
         </div>
-
-        {/* </Masonry> */}
-        {/* <ul className="grid grid-cols-3">
-          {images.map(({ src, alt, id }) => (
-            <li key={id}>
-              <Image
-                className="object-cover items-center mx-auto"
-                {...{ src, alt, width: imageSize, height: imageSize }}
-              />
-            </li>
-          ))}
-        </ul> */}
       </main>
     </>
   );
