5f2f58c82dc1340e1dcbe3bbf65bb74aebcff2bf
Stefan Rimaila <stefan@rimaila.fi>
2025-05-16 10:30:42 +0300

    feat: add front page layout

---
+++ src/layouts/FrontPageLayout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+export interface Props {
+  children?: ReactNode;
+}
+
+export default function FrontPageLayout(props: Props) {
+  return (
+    <main>
+      <h1>jfdsil</h1>
+    </main>
+  );
+}
